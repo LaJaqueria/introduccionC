d4ad88f6bba627c536b0bdf13ed92eb003c5e285
zerasul <zerasul@gmail.com>
2021-10-29 21:38:54 +0200

    añadiendo structs y makefile

---
+++ structs/mistruct.h
@@ -0,0 +1,5 @@
+#ifndef _H_MISTRUCT
+//TODO:definir un struct con edad, nombre y nacionalidad
+#define _H_MISTRUCT
+
+#endif
---
+++ structs/structs.c
@@ -0,0 +1,20 @@
+#include "mistruct.h"
+#include <stdio.h>
+
+#define MAXPERS 10
+
+int main(int argc, char* args){
+
+    Persona personas[MAXPERS];
+
+    
+    for(int i=0;i<MAXPERS;i++){
+        //TODO: almacenar 10 personas y guardarlo en el array
+    }
+
+    for(int j=0;j<MAXPERS;j++){
+        //TODO: mostrar las 10 personas con sus datos...
+    }
+
+    return 0;
+}
